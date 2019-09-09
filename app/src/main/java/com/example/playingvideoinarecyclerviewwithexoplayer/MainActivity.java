572aff4a071f80a22f22111d2c18e394cf692beb
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-09 21:55:09 +0530

    initial commit

---
+++ app/src/main/java/com/example/playingvideoinarecyclerviewwithexoplayer/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.playingvideoinarecyclerviewwithexoplayer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoPlayerRecyclerView mRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+
+        ArrayList<MediaObject> mediaObjects = new ArrayList<MediaObject>(Arrays.asList(Resources.MEDIA_OBJECTS));
+        mRecyclerView.setMediaObjects(mediaObjects);
+        VideoPlayerRecyclerAdapter adapter = new VideoPlayerRecyclerAdapter(mediaObjects, initGlide());
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private RequestManager initGlide() {
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background);
+
+        return Glide.with(this)
+                .setDefaultRequestOptions(options);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mRecyclerView != null)
+            mRecyclerView.releasePlayer();
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/playingvideoinarecyclerviewwithexoplayer/VideoPlayerRecyclerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.playingvideoinarecyclerviewwithexoplayer;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.RequestManager;
+
+import java.util.ArrayList;
+
+public class VideoPlayerRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private ArrayList<MediaObject> mediaObjects;
+    private RequestManager requestManager;
+
+
+    public VideoPlayerRecyclerAdapter(ArrayList<MediaObject> mediaObjects, RequestManager requestManager) {
+        this.mediaObjects = mediaObjects;
+        this.requestManager = requestManager;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new VideoPlayerViewHolder(
+                LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_video_list_item, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        ((VideoPlayerViewHolder) viewHolder).onBind(mediaObjects.get(i), requestManager);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mediaObjects.size();
+    }
+
+}
